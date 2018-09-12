a8c8f4a7269830f9ae920a2734588d774b833926
sol-idsnake <h.wendt@gmx.com>
2018-09-12 17:13:55 -0400

    added tests

--- src/components/addbase-test.js
+++ src/components/addbase-test.js
@@ -9,4 +9,22 @@ describe("<AddBase />", () => {
 	it("Renders without crashing", () => {
 		const wrapper = shallow(<AddBase />);
 	});
+
+	it("dispatches onAdd from onSubmit", () => {
+		const seedBase = [];
+		beforeAll(() => {
+			for (let i = 0; i < 10; i++) {
+				seedBase.push({
+					text: `Base ${i}`
+				});
+			}
+		});
+		const dispatch = jest.fn();
+		const wrapper = shallow(<AddBase dispatch={dispatch} />);
+
+		const instance = wrapper.instance();
+		const base = seedBase[0].text;
+		instance.onAdd(base);
+		expect(dispatch).toHaveBeenCalledWith(onAdd(base));
+	});
 });
